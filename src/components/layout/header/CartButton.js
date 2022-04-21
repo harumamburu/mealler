cb83f876dee109e6d0cfee6dacacef1bd31faad1
harumamburu <nikolay.kovalev92@gmail.com>
2022-04-21 19:33:04 +0300

    Add animation to the cart button

--- src/components/layout/header/CartButton.js
+++ src/components/layout/header/CartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -9,15 +9,29 @@ import styles from './CartButton.module.css';
 
 const CartButton = () => {
   const modalCtx = useContext(ModalContext);
-  const orderContext = useContext(OrderContext);
+  const orderCtx = useContext(OrderContext);
+  const [isButtonAnimated, setisButtonAnimated] = useState(false);
+
+  useEffect(() => {
+    if (orderCtx.positions.length === 0) {
+      return;
+    }
+    setisButtonAnimated(true);
+
+    const timer = setTimeout(() => setisButtonAnimated(false), 300);
+    return () => clearTimeout(timer);
+  }, [orderCtx.positions]);
 
   return (
-    <Button className={styles.cartbutton} onClick={() => modalCtx.setModal('cart', true)}>
+    <Button
+      className={`${styles.cartbutton} ${isButtonAnimated ? styles.bumpy : ''}`}
+      onClick={() => modalCtx.setModal('cart', true)}
+    >
       <span className={styles.icon}>
         <FontAwesomeIcon icon={faCartShopping} />
       </span>
       <span className={styles.title}>Your Cart</span>
-      <span className={styles.count}>{orderContext.totalAmount}</span>
+      <span className={styles.count}>{orderCtx.totalAmount}</span>
     </Button>
   );
 };
