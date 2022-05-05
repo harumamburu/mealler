e342457f158d22736682233af7bca1af15609d7a
harumamburu <nikolay.kovalev92@gmail.com>
2022-05-05 13:25:12 +0300

    Add checlout form styling

---
+++ src/components/checkout/checkout-form.config.ts
@@ -0,0 +1,123 @@
+import {
+  createFormFieldConfig,
+  doesntMatchRegexpRule,
+  FormConfig,
+  matchesRegexpRule,
+  requiredRule,
+} from '../../lib/form-util';
+import styles from './Checkout.module.css';
+
+const checkoutFormConfig: FormConfig = {
+  name: {
+    ...createFormFieldConfig(
+      'name',
+      (isValid) => `${styles.input} ${styles.required} ${isValid ? '' : styles.invalid}`,
+      'Name',
+      'checkout-name',
+      {
+        type: 'text',
+        maxLength: 70,
+      }
+    ),
+    validations: [
+      requiredRule('Name'),
+      doesntMatchRegexpRule(
+        'Name',
+        /[1-9!@#$%^&*()\-_=+[{\]};:\\|/?>,<~\]]+|^$/,
+        'No special characters/digits are allowed'
+      ),
+    ],
+  },
+  phone: {
+    ...createFormFieldConfig(
+      'phone',
+      (isValid) => `${styles.input} ${styles.required} ${isValid ? '' : styles.invalid}`,
+      'Phone',
+      'checkout-phone',
+      {
+        type: 'tel',
+        placeholder: '+1(888)123-4567',
+      }
+    ),
+    validations: [
+      requiredRule('Phone'),
+      matchesRegexpRule(
+        'Phone',
+        /^\+([0-9]){1,3}\(([0-9]){1,3}\)([0-9\-]){6,10}$/,
+        '+1(888)123-456 with other codes'
+      ),
+    ],
+  },
+  email: {
+    ...createFormFieldConfig(
+      'email',
+      (isValid) => `${styles.input} ${isValid ? '' : styles.invalid}`,
+      'Email',
+      'checkout-email',
+      {
+        type: 'email',
+        maxLength: 100,
+      }
+    ),
+    validations: [
+      matchesRegexpRule(
+        'Email',
+        /^[\w\.\-]+@[\w\.\-]+\.\w{1,4}$/,
+        "please make sure it's a valid e-mail"
+      ),
+    ],
+  },
+  street: {
+    ...createFormFieldConfig(
+      'street',
+      (isValid) => `${styles.input} ${styles.required} ${isValid ? '' : styles.invalid}`,
+      'Street',
+      'checkout-street',
+      {
+        type: 'text',
+        maxLength: 70,
+      }
+    ),
+    validations: [requiredRule('Street')],
+  },
+  house: {
+    ...createFormFieldConfig(
+      'house',
+      (isValid) => `${styles.input} ${styles.required} ${isValid ? '' : styles.invalid}`,
+      'House',
+      'checkout-house',
+      {
+        type: 'text',
+        maxLength: 5,
+      }
+    ),
+    validations: [requiredRule('House')],
+  },
+  appartment: {
+    ...createFormFieldConfig(
+      'appartment',
+      () => `${styles.input}`,
+      'Appartment',
+      'checkout-appartment',
+      {
+        type: 'text',
+        maxLength: 5,
+      }
+    ),
+    validations: [],
+  },
+  save: {
+    ...createFormFieldConfig(
+      'save',
+      () => `${styles.checkbox} ${styles.input}`,
+      'Remember Address',
+      'checkout-save',
+      {
+        type: 'checkbox',
+      }
+    ),
+    validations: [],
+  },
+};
+
+export default checkoutFormConfig;
--- src/components/checkout/checkout.tsx
+++ src/components/checkout/checkout.tsx
@@ -1,90 +1,23 @@
-import {
-  createFormFieldConfig,
-  doesntMatchRegexpRule,
-  FormConfig,
-  matchesRegexpRule,
-  requiredRule,
-} from '../../lib/form-util';
 import useForm from '../hooks/use-form';
+
+import checkoutFormConfig from './checkout-form.config';
 import Modal from '../ui/modal/Modal';
+import styles from './Checkout.module.css';
+import Button from '../ui/button/Button';
 
 const Checkout = () => {
-  const formConfig: FormConfig = {
-    name: {
-      ...createFormFieldConfig('name', () => '', 'Name', 'checkout-name', {
-        type: 'text',
-        maxLength: 70,
-      }),
-      validations: [
-        requiredRule('Name'),
-        doesntMatchRegexpRule(
-          'Name',
-          /[1-9!@#$%^&*()\-_=+[{\]};:\\|/?>,<~\]]+/,
-          'No special characters or digits are allowed'
-        ),
-      ],
-    },
-    phone: {
-      ...createFormFieldConfig('phone', () => '', 'Phone', 'checkout-phone', {
-        type: 'tel',
-        placeholder: '+1(888)123-4567',
-      }),
-      validations: [
-        requiredRule('Phone'),
-        matchesRegexpRule(
-          'Phone',
-          /^\+([0-9]){1,3}\(([0-9]){1,3}\)([0-9\-]){6,10}$/,
-          '+1(888)123-456 with other possible codes'
-        ),
-      ],
-    },
-    email: {
-      ...createFormFieldConfig('email', () => '', 'Email', 'checkout-email', {
-        type: 'email',
-        maxLength: 100,
-      }),
-      validations: [
-        matchesRegexpRule(
-          'Email',
-          /^(\w[\.\-])+@(\w[\.\-])+\.\w{1,4}$/,
-          "please make sure it's a valid e-mail"
-        ),
-      ],
-    },
-    street: {
-      ...createFormFieldConfig('street', () => '', 'Street', 'checkout-street', {
-        type: 'text',
-        maxLength: 70,
-      }),
-      validations: [requiredRule('Street')],
-    },
-    house: {
-      ...createFormFieldConfig('house', () => '', 'House', 'checkout-house', {
-        type: 'text',
-        maxLength: 5,
-      }),
-      validations: [requiredRule('House')],
-    },
-    appartment: {
-      ...createFormFieldConfig('appartment', () => '', 'Appartment', 'checkout-appartment', {
-        type: 'text',
-        maxLength: 5,
-      }),
-      validations: [],
-    },
-    save: {
-      ...createFormFieldConfig('save', () => '', 'Remember Address', 'checkout-save', {
-        type: 'checkbox',
-      }),
-      validations: [],
-    },
-  };
-  const [renderForm, ,] = useForm(formConfig);
+  const [renderForm, ,] = useForm(checkoutFormConfig);
 
   return (
-    <Modal>
+    <Modal cardClassName={styles.checkout}>
       <form>
-        <>{renderForm()}</>
+        <>
+          {renderForm()}
+          <div className={styles.controls}>
+            <Button>Cancel</Button>
+            <Button isMain>Confirm</Button>
+          </div>
+        </>
       </form>
     </Modal>
   );
