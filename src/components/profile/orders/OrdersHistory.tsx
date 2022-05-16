c2ca975ff64e6b65654457be4d6418376daa0ade
harumamburu <nikolay.kovalev92@gmail.com>
2022-05-16 12:17:06 +0300

    Change orders history markup to a table

--- src/components/profile/orders/OrdersHistory.tsx
+++ src/components/profile/orders/OrdersHistory.tsx
@@ -2,9 +2,11 @@ import { useContext, useEffect } from 'react';
 
 import AuthContext from '../../../store/auth-context';
 import { fetchOrders } from '../../../lib/api';
+import OrderedMeal from '../../../model/OrderedMeal';
 import Spinner from '../../ui/spinner/Spinner';
-import useHttp, { Status } from '../../../hooks/use-http';
 import SubmittedOrder from '../../../model/SubmittedOrder';
+import useHttp, { Status } from '../../../hooks/use-http';
+import styles from './OrdersHistory.module.css';
 
 const OrdersHistory = () => {
   const authContext = useContext(AuthContext);
@@ -13,16 +15,34 @@ const OrdersHistory = () => {
     httpCallback(authContext.userId);
   }, [httpCallback]);
 
-  const renderOrder = (order: SubmittedOrder, index: number) => {
+  const reduceHistory = (
+    history: SubmittedOrder[]
+  ): { [date: string]: [{ order: OrderedMeal[] }] } => {
+    return history.reduce((map, order) => {
+      if (!map[order.date]) {
+        map[order.date] = [] as unknown as [{ order: OrderedMeal[] }];
+      }
+      map[order.date].push({ order: order.positions });
+      return map;
+    }, {} as { [date: string]: [{ order: OrderedMeal[] }] });
+  };
+
+  const renderOrder = (date: string, orders: [{ order: OrderedMeal[] }]) => {
     return (
-      <li key={`order_${index}`}>
-        <span>{`${order.date}: `}</span>
-        <span>
-          {order.positions.map((position, index) => (
-            <p key={`order_${index}_${index}`}>{`x${position.amount} ${position.name}`}</p>
-          ))}
-        </span>
-      </li>
+      <>
+        <tr className={styles.date}>
+          <td colSpan={3}>{date}</td>
+        </tr>
+        {orders.map(({ order }, orderIndex) =>
+          order.map((position, positionIndex) => (
+            <tr key={`order_${date}_${orderIndex}_meal_${positionIndex}`}>
+              {positionIndex === 0 && <td rowSpan={order.length}>{`Order #${orderIndex + 1}`}</td>}
+              <td>{position.name}</td>
+              <td>{`x${position.amount}`}</td>
+            </tr>
+          ))
+        )}
+      </>
     );
   };
 
@@ -30,7 +50,20 @@ const OrdersHistory = () => {
     <>
       {status === Status.PENDING && <Spinner />}
       {error && <span>{error}</span>}
-      {data && <ul>{data.map(renderOrder)}</ul>}
+      {data && (
+        <div className={styles.history}>
+          <table>
+            <thead>
+              <th>Order</th>
+              <th>Position</th>
+              <th>Amount</th>
+            </thead>
+            <tbody>
+              {Object.entries(reduceHistory(data)).map(([key, value]) => renderOrder(key, value))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </>
   );
 };
