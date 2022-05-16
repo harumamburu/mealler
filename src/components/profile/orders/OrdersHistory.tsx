aa65b01a1994beb5f471f4f1fb78085d9fe55b1b
harumamburu <nikolay.kovalev92@gmail.com>
2022-05-16 13:01:28 +0300

    Add repeat order button to orders history

--- src/components/profile/orders/OrdersHistory.tsx
+++ src/components/profile/orders/OrdersHistory.tsx
@@ -1,18 +1,22 @@
-import { useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import AuthContext from '../../../store/auth-context';
 import { fetchOrders } from '../../../lib/api';
+import OrderContext from '../../../store/order-context';
 import OrderedMeal from '../../../model/OrderedMeal';
 import Spinner from '../../ui/spinner/Spinner';
 import SubmittedOrder from '../../../model/SubmittedOrder';
 import useHttp, { Status } from '../../../hooks/use-http';
 import styles from '../Profile.module.css';
+import Button from '../../ui/button/Button';
 
 const OrdersHistory = () => {
-  const authContext = useContext(AuthContext);
+  const authCtx = useContext(AuthContext);
+  const orderCtx = useContext(OrderContext);
   const { httpCallback, data, error, status } = useHttp(fetchOrders, true);
+
   useEffect(() => {
-    httpCallback(authContext.userId);
+    httpCallback(authCtx.userId);
   }, [httpCallback]);
 
   const reduceHistory = (
@@ -29,9 +33,9 @@ const OrdersHistory = () => {
 
   const renderOrder = (date: string, orders: [{ order: OrderedMeal[] }]) => {
     return (
-      <>
+      <React.Fragment key={`order_${date}`}>
         <tr className={styles.date}>
-          <td colSpan={3}>{date}</td>
+          <td colSpan={4}>{date}</td>
         </tr>
         {orders.map(({ order }, orderIndex) =>
           order.map((position, positionIndex) => (
@@ -39,10 +43,19 @@ const OrdersHistory = () => {
               {positionIndex === 0 && <td rowSpan={order.length}>{`Order #${orderIndex + 1}`}</td>}
               <td>{position.name}</td>
               <td>{`x${position.amount}`}</td>
+              {positionIndex === 0 && (
+                <td rowSpan={order.length} className={styles.centered}>
+                  <Button
+                    onClick={() => order.forEach((position) => orderCtx.addOrderPosition(position))}
+                  >
+                    Repeat Order
+                  </Button>
+                </td>
+              )}
             </tr>
           ))
         )}
-      </>
+      </React.Fragment>
     );
   };
 
@@ -54,9 +67,12 @@ const OrdersHistory = () => {
         <div className={styles.profile}>
           <table>
             <thead>
-              <th>Order</th>
-              <th>Position</th>
-              <th>Amount</th>
+              <tr>
+                <th>Order</th>
+                <th>Position</th>
+                <th>Amount</th>
+                <th></th>
+              </tr>
             </thead>
             <tbody>
               {Object.entries(reduceHistory(data)).map(([key, value]) => renderOrder(key, value))}
